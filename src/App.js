36d7ff45d4eaaf992f6eb88980ae453cf7bf86ef
Juanru <yz5026@nyu.edu>
2019-11-04 21:03:07 +0800

    finish basic structure with map()

--- src/App.js
+++ src/App.js
@@ -3,35 +3,62 @@ import './App.css';
 
 let shoppingCartItems = [
   {
+    item: 'Coconut Water',
+    image: 'http://g.search.alicdn.com/img/bao/uploaded/i4/i1/2436572716/O1CN01uoK38a1VvyBsThqrQ_!!0-item_pic.jpg',
+    description: 'Well known as great drinks invented a hundred years age, Yeshu Coconut milk severs you the authetic taste of earth when you travel in universe.',
+    price: '100 Marscoin',
+    promoSentence: 'You cannot live without it'
+  },
+  {
+    item: 'Coconut Chicken',
+    image: 'https://www.getreadyhk.com/images/stories/blog/mrmrsfoodhk/runyuan/runyuan_04.JPG',
+    description: 'Followed by coconut water, coconut chicken is also recognized as one of the greatest food in 2020s, you donnot want to miss it during your Mars trip',
+    price: '258 Marscoin',
+    promoSentence: 'Your life is incomplete without Coconut Chicken'
+  },
+  {
     item: 'War and Peace',
     image: 'https://images4.penguinrandomhouse.com/cover/9781400079988',
     description: 'War and Peace broadly focuses on Napoleon’s invasion of Russia in 1812 and follows three of the most well-known characters in literature',
-    price: 100
+    price: '999 Marscoin',
+    promoSentence: 'Reading helps you to sleep well'
+  },
+  
+  {
+    item: 'NYU Shanghai Sticker',
+    image: 'https://media1.giphy.com/media/j0GSxl5cuQKp3Rtmt1/source.gif',
+    description: 'This sticker gives you comfort and reminds you of great experience of NYU Shanghai although the school does not exist anymore',
+    price: '1000 Marscoin'
   },
 ]
 
 function App() {
   return (
     <div className="App">
-      <h1>Your Shopping Cart</h1>
-     <SearchForm />
+      <h1 className="heading">Life Essentials</h1>
+     {/* <SearchForm /> */}
      <ShoppingCart />
+     <p>Inspired by Yeshu Coconut</p>
     </div>
   );
 }
 
 function ShoppingCart() {
   return(
-    <div className="cart">
-      <Product />
-    </div>
+    shoppingCartItems.map(product)
   );
 }
 
-function Product() {
+function product(a) {
   return(
     <div className="product">
-      <img src={shoppingCartItems[0].image} width="100"/>
+      <div>
+      <img src={a.image} width="100"/>
+      <p className="nameOfProduct"> {a.item}</p>
+      <p className="description"> {a.description}</p>
+      <p className="promoSentence"> {a.promoSentence}</p>
+      <p className="price">{a.price}</p>
+      </div>
     </div>
   )
 }
